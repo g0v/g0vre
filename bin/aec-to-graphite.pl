75f822386180706207c58224c2355a5827173f23
Kang-min Liu <gugod@gugod.org>
2012-12-09 13:57:38 +0100

    allow using G0VRE_HOST to change g0vre server address.

--- bin/aec-to-graphite.pl
+++ bin/aec-to-graphite.pl
@@ -67,7 +67,7 @@ sub send_metrics {
 
 sub retrieve_from_jitsu {
     my $ua = LWP::UserAgent->new;
-    my $response = $ua->get("http://gugod.g0vre.jit.su/aec");
+    my $response = $ua->get("http://" . ($ENV{G0VRE_HOST} || "http://gugod.g0vre.jit.su") . "/aec");
     exit -1 unless $response->is_success;
     my $datum = decode_json $response->decoded_content;
     my $metrics = [];
--- bin/taipower-to-graphite.pl
+++ bin/taipower-to-graphite.pl
@@ -53,7 +53,7 @@ sub send_metrics {
 
 sub retrieve_from_jitsu {
     my $ua = LWP::UserAgent->new;
-    my $response = $ua->get("http://gugod.g0vre.jit.su/taipower");
+    my $response = $ua->get("http://" . ($ENV{G0VRE_HOST} || "gugod.g0vre.jit.su") . "/taipower");
     exit -1 unless $response->is_success;
     my $datum = decode_json $response->decoded_content;
     my $metrics = [];
