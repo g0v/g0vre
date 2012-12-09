77a8bff5aabe8c97ce2ae4403f3e6b404076df92
Kang-min Liu <gugod@gugod.org>
2012-12-09 11:16:16 +0100

    the program to send cwb.rainfall to graphite

---
+++ bin/cwb-to-graphite.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+use strict;
+use utf8;
+use JSON qw(decode_json);
+use Net::Graphite;
+use LWP::UserAgent;
+use DateTime::Format::ISO8601;
+
+sub send_metrics {
+    my $metrics = shift;
+    my $ng = Net::Graphite->new();
+    for my $metric (@$metrics) {
+        utf8::encode($metric->{path});
+        utf8::encode($metric->{value});
+        utf8::encode($metric->{time});
+        my $plaintext = $ng->send(%$metric);
+        print $plaintext;
+    }
+}
+
+sub retrieve_from_jitsu {
+    my $ua = LWP::UserAgent->new;
+    my $response = $ua->get("http://gugod.g0vre.jit.su/cwb.rainfall");
+    exit -1 unless $response->is_success;
+    my $datum = decode_json $response->decoded_content;
+    my $metrics = [];
+    for my $data (@$datum) {
+        my $metric = "cwb.rainfall.10." . $data->{station};
+        my $t = DateTime::Format::ISO8601->parse_datetime( $data->{time} )->epoch;
+        my $value = $data->{value};
+        if (defined $value) {
+            push @$metrics, {
+                path  => $metric,
+                value => $value,
+                time  => $t
+            }
+        }
+    }
+
+    return $metrics;
+}
+
+send_metrics retrieve_from_jitsu;
