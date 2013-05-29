eea31eff597c20991fe89aedf48f6a1aaba456c6
Kang-min Liu <gugod@gugod.org>
2013-05-29 23:50:38 +0200

    change default g0vre server.

--- bin/taipower-to-graphite.pl
+++ bin/taipower-to-graphite.pl
@@ -53,7 +53,7 @@ sub send_metrics {
 
 sub retrieve_from_jitsu {
     my $ua = LWP::UserAgent->new;
-    my $response = $ua->get("http://" . ($ENV{G0VRE_HOST} || "gugod.g0vre.jit.su") . "/taipower");
+    my $response = $ua->get("http://" . ($ENV{G0VRE_HOST} || "g0vre.heroku.com") . "/taipower");
     exit -1 unless $response->is_success;
     my $datum = decode_json $response->decoded_content;
     my $metrics = [];
