e8496fd3027b5c1cf8f04dc04d3b8c7fad7eb673
Kang-min Liu <gugod@gugod.org>
2015-08-08 20:49:33 +0800

    send all rainfall data to graphite

--- bin/cwb-to-graphite.pl
+++ bin/cwb-to-graphite.pl
@@ -20,19 +20,22 @@ sub send_metrics {
 
 sub retrieve_from_jitsu {
     my $ua = LWP::UserAgent->new;
-    my $response = $ua->get("http://" . ($ENV{G0VRE_HOST} || "g0vre.herokuapp.com") . "/cwb.rainfall");
+    my $response = $ua->get("http://" . ($ENV{G0VRE_HOST} || "g0vre.herokuapp.com") . "/cwb.rains");
     exit -1 unless $response->is_success;
     my $datum = decode_json $response->decoded_content;
     my $metrics = [];
+    my @fields = ("10","60","180","360","720","1800","today","previous-day","last-two-days");
     for my $data (@$datum) {
-        my $metric = "cwb.rainfall10." . $data->{station};
-        my $t = DateTime::Format::ISO8601->parse_datetime( $data->{time} )->epoch;
-        my $value = $data->{value};
-        if (defined $value) {
-            push @$metrics, {
-                path  => $metric,
-                value => $value,
-                time  => $t
+        for my $i (0..$#fields) {
+            my $field_name = $fields[$i];
+            my $metric_name = "cwb.rainfall.${field_name}." . $data->{station};
+            if (defined(my $metric_value = $data->{values}[$i])) {
+                my $t = DateTime::Format::ISO8601->parse_datetime( $data->{time} )->epoch;
+                push @$metrics, {
+                    path  => $metric_name,
+                    value => $metric_value,
+                    time  => $t
+                }
             }
         }
     }
